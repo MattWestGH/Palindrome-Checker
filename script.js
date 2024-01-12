2ce139db2a8d86f425135a952084462eb8cf515d
Matt West <95767818+MattWestGH@users.noreply.github.com>
2024-01-12 18:47:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const checkBtn = document.getElementById("check-btn");
 const userInput = document.getElementById("text-input");
 const resultText= document.getElementById("result")
 
-checkBtn.addEventListener("click", ()=>{
-  if(userInput.value===""){
-    alert("Please input a value");
-    }
-  else if(userInput.value.length%2 === 1){
-     if(userInput.value.length === 1){
-      resultText.textContent = `${userInput.  value} is a palindrome`}
-}else if(userInput.value.length%2 === 0){
-  alert("not doing this one yet")
+checkBtn.addEventListener("click", btnClicked)
+
+function btnClicked(){
+  if(userInput.value===''){
+    alert('Please input a value')
+  }else if(checkPalindrome(userInput.value) === true){
+    resultText.innerText = `${userInput.value} is a palindrome`
+  }else if(checkPalindrome(userInput.value) ===false){
+    resultText.innerText = `${userInput.value} is not a palindrome`
+  }
 }
-console.log(userInput.value)
-})
+
+function checkPalindrome(userInput){
+  const regex = /[\W_]/g
+  let cleanedInput = userInput.toLowerCase().replace(regex, '');
+  let reversedInput = cleanedInput.split("").reverse().join("");
+  return reversedInput === cleanedInput
+}
